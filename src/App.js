26bc08729ec81edda111e06a116cdd08dc27875d
Vinícios Grama <vinyirun1@gmail.com>
2019-10-27 23:09:40 -0300

    :sparkles: Inicia BD

--- src/App.js
+++ src/App.js
@@ -3,10 +3,31 @@ import React from 'react';
 import {ThemeContextProvider} from './core/ThemeProvider';
 import Navigator from './Navigator';
 
+import {Database} from '@nozbe/watermelondb';
+import DatabaseProvider from '@nozbe/watermelondb/DatabaseProvider';
+import SQLiteAdapter from '@nozbe/watermelondb/adapters/sqlite';
+
+import {mySchema} from './models/schema';
+import Activity from './models/Activity';
+import SubActivity from './models/SubActivity';
+
+const adapter = new SQLiteAdapter({
+  dbName: 'TodoList',
+  schema: mySchema,
+});
+
+const database = new Database({
+  adapter,
+  modelClasses: [Activity, SubActivity],
+  actionsEnabled: true,
+});
+
 export default function App() {
   return (
-    <ThemeContextProvider>
-      <Navigator />
-    </ThemeContextProvider>
+    <DatabaseProvider database={database}>
+      <ThemeContextProvider>
+        <Navigator />
+      </ThemeContextProvider>
+    </DatabaseProvider>
   );
 }
