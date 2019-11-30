4d9bce377ff1afaafb9ad81cc453872433813c6d
Vinícios Grama <vinyirun1@gmail.com>
2019-11-30 17:48:28 -0300

    Adiciona mais cores aos temas

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,15 +1,23 @@
 export const dark = {
   mode: 'DARK',
   PRIMARY_COLOR: '#121212',
+  SECONDARY: '#2C2C2C',
   TEXT_COLOR: '#D8DEE9',
   ACTIVE_COLOR: '#121212',
   INACTIVE_COLOR: '#202020',
+  INPUT: '#4F4F4F',
+  INPUT_PLACEHOLDER: '#A0A0A0',
+  SAVE: '#00D3BF',
 };
 
 export const light = {
   mode: 'LIGHT',
-  PRIMARY_COLOR: '#ffffff',
+  PRIMARY_COLOR: '#fff',
+  SECONDARY: '#F7F7F7',
   TEXT_COLOR: '#000',
   ACTIVE_COLOR: '#E7E7E7',
   INACTIVE_COLOR: '#fff',
+  INPUT: '#ECECEC',
+  INPUT_PLACEHOLDER: '#A0A0A0',
+  SAVE: '#3B00BF',
 };
