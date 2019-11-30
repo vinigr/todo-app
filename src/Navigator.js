936f385b51055b830e9b852745e77b9550cc042a
Vinícios Grama <vinyirun1@gmail.com>
2019-11-30 17:48:19 -0300

    Adiciona tela a navegação

--- src/Navigator.js
+++ src/Navigator.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import {createAppContainer} from 'react-navigation';
 import {createBottomTabNavigator} from 'react-navigation-tabs';
+import {createStackNavigator} from 'react-navigation-stack';
+import {fadeIn} from 'react-navigation-transitions';
+
 import Home from './screens/Home/Home';
+import NewActivity from './screens/NewActivity/NewActivity';
+
 import Settings from './screens/Settings/Settings';
 import TabBar from './components/TabBar/TabBar';
 
@@ -9,10 +14,30 @@ import {withTheme} from 'styled-components';
 
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
-const TabNavigator = createBottomTabNavigator(
+const StackHome = createStackNavigator(
   {
     Home: {
       screen: Home,
+      navigationOptions: {
+        header: null,
+      },
+    },
+    NewActivity: {
+      screen: NewActivity,
+      navigationOptions: {
+        header: null,
+      },
+    },
+  },
+  {
+    transitionConfig: () => fadeIn(),
+  },
+);
+
+const TabNavigator = createBottomTabNavigator(
+  {
+    Home: {
+      screen: StackHome,
       navigationOptions: ({screenProps}) => ({
         tabBarIcon: (
           <Icon color={screenProps.theme.TEXT_COLOR} size={30} name="home" />
