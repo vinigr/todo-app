f380ae6efe3ee4fc18be6e5c4cabef07069249b7
Vinícios Grama <vinyirun1@gmail.com>
2019-11-30 17:48:07 -0300

    Melhora tela Home

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,13 +1,54 @@
-import React from 'react';
-import {View, Text, StatusBar, TouchableOpacity} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {StatusBar, TouchableOpacity} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import DateTimePickerModal from 'react-native-modal-datetime-picker';
 
-import {Container, Data, Dia, DataText, Tasks} from './styles';
+import {
+  Container,
+  Header,
+  Data,
+  ButtonAdd,
+  Dia,
+  DataText,
+  Tasks,
+  TaskText,
+  Task,
+} from './styles';
 
 import pt from 'date-fns/locale/pt';
 import format from 'date-fns/format';
 
+import {Activities} from '../../database';
+
 const Home = props => {
+  const [activities, setActivities] = useState([]);
+  const [date, setDate] = useState(new Date());
+
+  const [dateVisible, setDateVisible] = useState(false);
+
+  useEffect(() => {
+    const activitiesData = Activities.data();
+    setActivities(
+      activitiesData.filter(
+        activity =>
+          format(activity.screduledAt, 'd/M/y') === format(date, 'd/M/y'),
+      ),
+    );
+  }, [date]);
+
+  const showDateTimePicker = () => {
+    setDateVisible(true);
+  };
+
+  const hideDatePicker = () => {
+    setDateVisible(false);
+  };
+
+  const handleConfirm = value => {
+    setDate(value);
+    hideDatePicker();
+  };
+
   const {screenProps} = props;
 
   const colorStatus =
@@ -19,22 +60,53 @@ const Home = props => {
         backgroundColor={screenProps.theme.PRIMARY_COLOR}
         barStyle={colorStatus}
       />
-      <Data>
-        <Dia>{format(new Date(), 'EEEE', {locale: pt})}</Dia>
-        <DataText>{format(new Date(), ', d LLL', {locale: pt})}</DataText>
-      </Data>
+      <Header>
+        <Data onPress={showDateTimePicker}>
+          <Dia>{format(date, 'EEEE', {locale: pt})}</Dia>
+          <DataText>{format(date, ', d LLL', {locale: pt})}</DataText>
+        </Data>
+        <ButtonAdd onPress={() => props.navigation.navigate('NewActivity')}>
+          <Icon name="plus" color={screenProps.theme.PRIMARY_COLOR} size={30} />
+        </ButtonAdd>
+      </Header>
+
       <Tasks>
-        <TouchableOpacity>
-          <Icon name="check-circle-outline" color="#00cc08" size={30} />
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Icon
-            name="checkbox-blank-circle-outline"
-            color={screenProps.theme.TEXT_COLOR}
-            size={30}
-          />
-        </TouchableOpacity>
+        {activities.map(activity => (
+          <Task key={activity.id}>
+            {activity.completed ? (
+              <>
+                <TouchableOpacity
+                  onLongPress={() => console.log('segura')}
+                  onPress={() => console.log('click')}>
+                  <Icon name="check-circle-outline" color="#00cc08" size={30} />
+                </TouchableOpacity>
+                <TaskText>{activity.title}</TaskText>
+              </>
+            ) : (
+              <>
+                <TouchableOpacity>
+                  <Icon
+                    name="checkbox-blank-circle-outline"
+                    color={screenProps.theme.TEXT_COLOR}
+                    size={30}
+                  />
+                </TouchableOpacity>
+                <TaskText>{activity.title}</TaskText>
+              </>
+            )}
+          </Task>
+        ))}
       </Tasks>
+      <DateTimePickerModal
+        date={date}
+        value={date}
+        isVisible={dateVisible}
+        isDarkModeEnabled={true}
+        mode="date"
+        is24Hour={true}
+        onConfirm={handleConfirm}
+        onCancel={hideDatePicker}
+      />
     </Container>
   );
 };
--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -6,21 +6,50 @@ export const Container = styled.View`
   background-color: ${props => props.theme.PRIMARY_COLOR};
 `;
 
-export const Data = styled.View`
+export const Header = styled.View`
   flex-direction: row;
+  justify-content: space-between;
+  margin-bottom: 20px;
+`;
+
+export const ButtonAdd = styled.TouchableOpacity`
+  height: 40px;
+  width: 40px;
+  align-items: center;
+  justify-content: center;
+  background-color: ${props => props.theme.TEXT_COLOR};
+  border-radius: 20px;
+`;
+
+export const Data = styled.TouchableOpacity`
+  flex-direction: row;
+  align-items: flex-end;
 `;
 
 export const Dia = styled.Text`
-  font-size: 24px;
+  font-size: 28px;
   font-weight: bold;
   color: ${props => props.theme.TEXT_COLOR};
 `;
 
 export const DataText = styled.Text`
-  font-size: 24px;
+  font-size: 26px;
   color: ${props => props.theme.TEXT_COLOR};
 `;
 
-export const Tasks = styled.View`
+export const Tasks = styled.ScrollView`
   padding: 10px;
+  flex: 1;
+`;
+
+export const Task = styled.View`
+  padding: 10px;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const TaskText = styled.Text`
+  font-size: 26px;
+  margin-left: 10px;
+  color: ${props => props.theme.TEXT_COLOR};
 `;
